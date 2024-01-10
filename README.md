4e734309077a519533741980eec03494c2320ad4
maylique <supercoder@AirvonSperCoder.fritz.box>
2024-01-10 11:27:38 +0100

    logik fehlerhaft

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Todo-List
+
+#Demo: https://maylique.github.io/Todo-List/
+
+Eine Todo Liste welche mithilfe von Javascript realisiert wurde.
---
+++ assets/javascript/main.js
@@ -0,0 +1,56 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+let array = []
+
+
+const inputToArray = () => {
+    array.push({ input: input.value, status: false });
+    console.log(array);
+}
+
+const arrayToDiv = (array) => {
+    array.forEach(el => {
+        const div = document.createElement('div')
+        const checkbox = document.createElement('input')
+        const p = document.createElement('p')
+        const deleteBtn = document.createElement('button')
+    
+        checkbox.setAttribute('type', 'checkbox')
+        console.log(checkbox);
+
+        if (checkbox.checked) {
+            p.style.textDecoration = 'line-through'
+        } else {
+            
+        }
+
+        p.textContent = el.input
+        deleteBtn.textContent = 'X'
+
+        deleteBtn.addEventListener('click', function () {
+            const div = this.parentNode;
+            const previousSiblings = [];
+        
+            let previousSibling = div.previousSibling;
+            while (previousSibling) {
+                if (previousSibling.nodeType === 1) {
+                    previousSiblings.push(previousSibling);
+                }
+                previousSibling = previousSibling.previousSibling;
+            }
+        
+            div.remove();
+        });
+    
+        output.appendChild(div)
+        div.appendChild(checkbox)
+        div.appendChild(p)
+        div.appendChild(deleteBtn)
+    });
+}
+
+const combine = (x) => {
+    output.innerHTML = ''
+    inputToArray()
+    arrayToDiv(x)
+}
