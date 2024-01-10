35b60b6116470c1211cee9c1b08d64bda6e20338
Maylique <maylique14@gmail.com>
2024-01-10 13:43:51 +0100

    javascript done?

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,8 +4,13 @@ let array = []
 
 
 const inputToArray = () => {
-    array.push({ input: input.value, status: false });
-    console.log(array);
+    if (input.value >= 1) {
+        array.push({ text: input.value, done: false });
+    }
+}
+
+const clearInput = () => {
+    input.value = ''
 }
 
 const arrayToDiv = (array) => {
@@ -16,20 +21,29 @@ const arrayToDiv = (array) => {
         const deleteBtn = document.createElement('button')
     
         checkbox.setAttribute('type', 'checkbox')
-        console.log(checkbox);
 
-        if (checkbox.checked) {
-            p.style.textDecoration = 'line-through'
-        } else {
-            
-        }
+        checkbox.addEventListener('change', function () {
+            if (this.checked) {
+                p.style.textDecoration = 'line-through'
+            } else {
+                p.style.textDecoration = 'none'
+            }
+        });
 
-        p.textContent = el.input
+
+        p.textContent = el.text
         deleteBtn.textContent = 'X'
 
         deleteBtn.addEventListener('click', function () {
+            const checkbox = this.parentNode.querySelector('input[type="checkbox"]');
+            if (!checkbox.checked) {
+                alert('Nix da alda');
+                return;
+            }
             const div = this.parentNode;
             div.remove();
+            const index = array.findIndex(item => item.input === el.input);
+            array.splice(index, 1);
         });
     
         output.appendChild(div)
@@ -37,10 +51,14 @@ const arrayToDiv = (array) => {
         div.appendChild(p)
         div.appendChild(deleteBtn)
     });
+
 }
 
 const combine = (x) => {
     output.innerHTML = ''
     inputToArray()
     arrayToDiv(x)
+    clearInput()
 }
+
+
